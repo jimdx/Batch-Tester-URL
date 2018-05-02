a9255fe56639387cb37405f9b08ddc4b90b76836
webingio <36809253+webingio@users.noreply.github.com>
2018-05-02 23:25:52 +0800

    Create url.py

---
+++ url.py
@@ -0,0 +1,34 @@
+import requests
+import sys
+import threading
+import queue
+
+list_name = sys.argv[1]
+num = int(sys.argv[2])
+
+quit = queue.Queue()
+threading_num = num
+
+url_list = open(list_name,'r')
+lines = url_list.readlines()
+url_list.close()
+for line in lines:
+    line = line.rstrip()
+    quit.put(line)
+
+
+def crawler():
+    while not quit.empty():
+        url = quit.get()
+        try:
+            requests.packages.urllib3.disable_warnings()
+            content = requests.get(url, verify=False, allow_redirects=True, timeout=10)
+            if content.status_code == 200:
+                print (url)
+        except requests.RequestException as e:
+            pass
+
+if __name__ == '__main__':
+    for i in range(threading_num):
+        t = threading.Thread(target=crawler)
+        t.start()
